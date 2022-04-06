15ce3d424f6e4f41ea1d923b9aee35ca5674db9b
kyelebeniga <kyele.beniga@gmail.com>
2022-04-06 13:56:02 +0800

    Created login page

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+    @include 'config.php';   
+
+    $error = "";
+    if(isset($_POST['submit_login'])){
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
+
+        $mysql = "SELECT * FROM registration WHERE username='".$username."'AND password='".$password."' limit 1";
+        $result = mysqli_query($conn, $mysql);
+       
+        if(mysqli_num_rows($result)==1){
+            header('location:index.php');
+        }
+        else{
+            $error = "Invalid credentials.";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MHS | Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <section class="main">
+        <div class="login-container">
+            <h1>Login</h1>
+            <form method="post" class="login">
+                <input type="text" id="user" name="user" placeholder="Username">
+                <input type="password" id="pass" name="pass" placeholder="Password">
+                <div><span class="error"><?php echo $error; ?></span></div>
+                <input type="submit" class="btn" name="submit_login" value="Login">
+                <p>Not a member? <a href="#">Signup now</a></p>
+            </form>
+        </div>
+    </section>  
+</body>
+</html>
