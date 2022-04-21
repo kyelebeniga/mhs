b3c2d7852ca60b9726f76bd323f825a9f103fba5
kyelebeniga <kyele.beniga@gmail.com>
2022-04-21 18:00:22 +0800

    Added AJAX to signup
    -Needs unique username authentication

---
+++ php/signup.php
@@ -0,0 +1,12 @@
+<?php
+    $conn = mysqli_connect('localhost', 'root', '', 'mhs_db');
+
+    session_start();
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+    $hashedPass = password_hash($password, PASSWORD_DEFAULT);
+    
+    $mysql = "INSERT INTO registration (username, password) VALUES ('$username', '$hashedPass')";
+    $result = mysqli_query($conn, $mysql);
+
+?>
--- signup.php
+++ signup.php
@@ -1,45 +1,10 @@
-<?php
-    @include 'config.php';   
-
-    session_start();
-
-    $error = "";
-    if(isset($_POST['submit_login'])){
-        $username = $_POST['user'];
-        $password = $_POST['pass'];
-        $hashedPass = password_hash($password, PASSWORD_DEFAULT);
-
-        if(empty($username) || empty($password)){
-            $error = "Fill out all the blanks";
-        }
-
-        $query = mysqli_query($conn, "SELECT * FROM registration WHERE username ='".$username."'");
-        if(mysqli_num_rows($query) > 0){
-            $error = "User already exists!";
-        }
-        else{
-            $mysql = "INSERT INTO registration (username, password) VALUES ('$username', '$hashedPass')";
-            $result = mysqli_query($conn, $mysql);
-            echo "<script type='text/javascript'></script>";
-
-            if($result){
-                echo "<script>
-                    alert('Successfully registered!');
-                    window.open('login.php','_self');
-                </script>";
-            }
-            else{
-                echo "<script type='text/javascript'>alert('Error signing up!');</script>";
-            }
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <meta charset="UTF-8">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/notify.js"></script>
     <title>MHS | Register</title>
     <link rel="stylesheet" href="css/login.css">
 </head>
@@ -48,13 +13,43 @@
         <div class="login-container">
             <h1>Signup</h1>
             <form method="post" class="login">
-                <input type="text" id="user" name="user" placeholder="Username">
-                <input type="password" id="pass" name="pass" placeholder="Password">
-                <div><span class="error"><?php echo $error; ?></span></div>
+                <input type="text" id="user" name="user" class="user" placeholder="Username">
+                <input type="password" id="pass" name="pass" class="pass" placeholder="Password">
                 <input type="submit" class="btn" name="submit_login" value="Signup">
                 <p><a href="login.php">Cancel</a></p>
             </form>
         </div>
-    </section>  
+    </section> 
+    <script>
+        $(document).ready(function(){
+            $("form").on("submit", function(e){
+                e.preventDefault();
+
+                var username = $('#user').val();
+                var password = $('#pass').val();
+                
+                if(username == ''){
+                    $('.user').notify('Empty username.',
+                        {position: 'right'}
+                    );
+                }
+                else if(password == ''){
+                    $('.pass').notify('Empty password.',
+                        {position: 'right'}
+                    );
+                }
+                else{
+                    var formValues= $(this).serialize();
+            
+                    $.post("php/signup.php", formValues, function(data){
+                    $('.btn').notify('Success!', 'success',);
+                        setTimeout(function() { 
+                            window.location.href = 'login.php'; 
+                        }, 2000);
+                    });
+                }
+            });
+        });
+    </script>
 </body>
 </html>
